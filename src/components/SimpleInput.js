677827fa08c0f99dfe070f50dcae10308e2afe8f
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-10-25 16:13:07 -0400

    add custom hooks for input handling and validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,40 +1,46 @@
-import { useState } from "react";
+import useInput from '../hooks/use-input';
 
 const SimpleInput = (props) => {
+	const { 
+		value: enteredName, 
+		isValid: enteredNameIsValid,
+		valueChangeHandler: nameChangeHandler,
+		inputBlurHandler: nameBlurHandler,
+		hasError: nameInputHasError,
+		reset: resetNameInput
+	} = useInput(value => value.trim() !== '');
 
-	const [enteredName, setEnteredName] = useState('');
-	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+	const { 
+		value: enteredEmail, 
+		isValid: enteredEmailIsValid,
+		valueChangeHandler: emailChangeHandler,
+		inputBlurHandler: emailBlurHandler,
+		hasError: emailInputHasError,
+		reset: resetEmailInput
+	} = useInput(value => value.includes('@'));
 
-	const enteredNameIsValid = enteredName.trim() !== '';
-	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
-	
 	let formIsValid = false;
-	if (enteredNameIsValid) {
+	if (enteredNameIsValid && enteredEmailIsValid) {
 		formIsValid = true;
 	}
 
-	const nameInputChangeHandler = (event) => {
-		setEnteredName(event.target.value);
-	};
-
-	const nameInputBlurHandler = (event) => {
-		setEnteredNameTouched(true);
-	};
-
 	const formSubmissionHandler = (event) => {
 		event.preventDefault();
-		setEnteredNameTouched(true);
 
-		if(!enteredNameIsValid) {
+		if(!enteredNameIsValid || !enteredEmailIsValid) {
 			return
 		};
 		
 		console.log(enteredName);
-		setEnteredName('')
-		setEnteredNameTouched(false);
+		console.log(enteredEmail);
+
+		resetNameInput();
+		resetEmailInput();
 	}
 
-	const nameInputClasses = !nameInputIsInvalid ? 'form-control' : 'form-control invalid'
+	const nameInputClasses = !nameInputHasError ? 'form-control' : 'form-control invalid';
+	const emailInputClasses = !emailInputHasError? 'form-control' : 'form-control invalid';
+
 	return (
 		<form onSubmit={formSubmissionHandler}>
 			<div className={nameInputClasses}>
@@ -42,10 +48,20 @@ const SimpleInput = (props) => {
 				<input
 					type="text" id="name" 
 					value={enteredName} 
-					onChange={nameInputChangeHandler}
-					onBlur={nameInputBlurHandler}
+					onChange={nameChangeHandler}
+					onBlur={nameBlurHandler}
 				/>
-					{ nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
+					{ nameInputHasError && <p className="error-text">Name must not be empty</p>}
+			</div>
+			<div className={emailInputClasses}>
+				<label htmlFor="email">Email</label>
+				<input
+					type="email" id="email"
+					value={enteredEmail}
+					onChange={emailChangeHandler}
+					onBlur={emailBlurHandler}
+				/>
+				{ emailInputHasError && <p className="error-text">Please enter a valid email</p>}
 			</div>
 			<div className="form-actions">
 				<button disabled={!formIsValid}>Submit</button>
